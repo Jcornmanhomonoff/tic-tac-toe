9a0b26f7252dc658155a29cdac7970d69b8a3b62
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2016-04-06 10:42:34 -0400

    Switch between players on click

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,16 +1,31 @@
 'use strict';
 
 let turnClick = 0;
+//
+let players = ['player-x', 'player-o'];
+let player = '';
+
 
 //counts clicks
 $('.available').on('click', function(){
   let currentCell = $(this);
-    return turnClick++;
+    turnClick++;
+    if (turnClick % 2 === 0) {
+      player = players[0];
+    } else {
+      player = players[1];
+    }
 });
 
-//changes block to red on click
+//changes block color on click & alternates player
 $('.available').on('click', function(){
   let currentCell = $(this);
   currentCell.removeClass('available');
-  currentCell.css('background', 'red');
-});
+  currentCell.addClass('player');
+  if (player === 'player-x') {
+    currentCell.addClass('player-x');
+  }else {
+    currentCell.addClass('player-o');
+    }
+  }
+);
