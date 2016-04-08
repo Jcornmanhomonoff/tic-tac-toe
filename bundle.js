d852f2a9fb5b0ef2615a68eb8df8937ab63a41c6
jcornmanhomonoff <jcornmanhomonoff@gmail.com>
2016-04-07 23:08:56 -0400

    add bundle

--- bundle.js
+++ bundle.js
@@ -66,7 +66,7 @@ webpackJsonp([0],[
 	    var win = false;
 	    // console.log(player);
 	    if (player === gameBoard[0] && player === gameBoard[1] && player === gameBoard[2] || player === gameBoard[3] && player === gameBoard[4] && player === gameBoard[5] || player === gameBoard[6] && player === gameBoard[7] && player === gameBoard[8] || player === gameBoard[0] && player === gameBoard[3] && player === gameBoard[6] || player === gameBoard[1] && player === gameBoard[4] && player === gameBoard[7] || player === gameBoard[2] && player === gameBoard[5] && player === gameBoard[8] || player === gameBoard[0] && player === gameBoard[4] && player === gameBoard[8] || player === gameBoard[2] && player === gameBoard[4] && player === gameBoard[6]) {
-	      console.log("You win " + player);
+	      $('#Winner').text("You win " + player);
 	      win = true;
 	      $('.box').off('click');
 	    }
@@ -148,6 +148,10 @@ webpackJsonp([0],[
 	  //   event.preventDefault();
 	  //   authApi.signIn(authUi.signInSuccess, authUi.failure, data);
 	  // });
+	  $('#all-games').on('click', function (event) {
+	    event.preventDefault();
+	    authApi.allGames(authUi.allGamesSuccess, authUi.failure);
+	  });
 	  $('#sign-out').on('submit', function (event) {
 	    event.preventDefault();
 	    authApi.signOut(authUi.signOutSuccess, authUi.failure);
@@ -329,6 +333,17 @@ webpackJsonp([0],[
 	  }).done(success).fail(failure);
 	};
 
+	var allGames = function allGames(success, failure) {
+	  // debugger;
+	  $.ajax({
+	    method: 'GET',
+	    url: app.api + '/games',
+	    headers: {
+	      Authorization: 'Token token=' + app.user.token
+	    }
+	  }).done(success).fail(failure);
+	};
+
 	// const getUser = (success, failure, data) => {
 	//   // debugger;
 	//   $.ajax({
@@ -361,7 +376,8 @@ webpackJsonp([0],[
 	  changePassword: changePassword,
 	  createGame: createGame,
 	  joinGame: joinGame,
-	  updateGame: updateGame
+	  updateGame: updateGame,
+	  allGames: allGames
 	};
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
@@ -381,7 +397,7 @@ webpackJsonp([0],[
 /* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
 	var app = __webpack_require__(6);
 
@@ -401,6 +417,10 @@ webpackJsonp([0],[
 	  console.log(app);
 	};
 
+	var allGamesSuccess = function allGamesSuccess(data) {
+	  $('#history').text(data.games.length);
+	};
+
 	var success = function success(data) {
 	  console.log(data);
 	};
@@ -415,8 +435,10 @@ webpackJsonp([0],[
 	  signInSuccess: signInSuccess,
 	  signOutSuccess: signOutSuccess,
 	  createGameSuccess: createGameSuccess,
+	  allGamesSuccess: allGamesSuccess,
 	  app: app
 	};
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ },
 /* 8 */
